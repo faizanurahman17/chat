a48400b67ed6dd082a8f8358fde04e9118015300
Faizanur Rahman <faizanurahman1711@gmail.com>
2025-05-13 23:56:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    socket.on('msg', (msgs) => {
+        io.emit("msgs",msgs);
+    });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+    return res.sendFile("/public/index.html");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`server is running at port:${PORT}`))
