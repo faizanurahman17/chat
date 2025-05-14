d8d713474904f792409166af87fcef1616e47abb
Faizanur Rahman <faizanurahman1711@gmail.com>
2025-05-14 06:29:36 +0530

    back

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.get("/", (req, res) => {
     return res.sendFile("/public/index.html");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`server is running at port:${PORT}`))
